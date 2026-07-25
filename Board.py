b16db7f90162fe435f40ee3483472b877888d54d
Seth Maxwell <seth.maxwell@student.manchester.ac.uk>
2026-07-25 20:33:31 +0100

    Add validation to board

--- Board.py
+++ Board.py
@@ -1,9 +1,8 @@
-import csv
 import math
 
 
 class Board:
-    def __init__(self, grid=[], filename=""):
+    def __init__(self, grid=None, filename=""):
         '''
         Instantiate a grid object based on:
         A. A pre-populated 2d int list
@@ -11,50 +10,64 @@ class Board:
         '''
 
         # Ensure either a grid or filename is given
-        if (filename == ""):
-            if (grid == []):
-                raise Exception("No grid or filename given")
+        if (filename == "" and grid is None):
+            raise ValueError("No grid or filename given")
 
-        # Read CSV file into 2d list
+        # Read file into 2d list
         else:
-            with open(filename, "r") as csvfile:
-                reader = csv.reader(csvfile)
-                grid = list(reader)
+            grid = []
+            with open(filename, "r") as file:
+                for line in file:
+                    grid.append([int(num) for num in line.split(" ")])
 
         # Ensure grid is valid
-        if (not self.validate(grid)):
-            raise Exception("Grid invalid")
+        valid, reason = self.validate(grid)
+        if (not valid):
+            raise ValueError(f"Grid invalid: {reason}")
 
         # Save attributes
         self.grid = grid
         self.size = len(grid)
 
-    def validate(self, grid):
+    @staticmethod
+    def validate(grid):
         '''
         Ensure grid follows rules
         1. grid is a 2d int list
         2. width and height equal
         3. width and height a square number
-        4. every value between 0 and 9 (inclusive)
+        4. every value between 0 and size (inclusive)
         '''
 
-        # Ensure datastructure correct
-        if (type(grid) != list[list[int]]):
-            return False
+        # Ensure at least list
+        if (not isinstance(grid, list)):
+            return False, f"Top level not a list, {type(grid)}"
 
         # Ensure size is square
         lines = len(grid)
-        if (math.sqrt(lines) != math.floor(math.sqrt(lines))):
-            return False
+        root = math.isqrt(lines)
+        if (root * root != lines):
+            return False, "Num lines not square"
 
         for line in grid:
+            # Ensure 2d list
+            if (not isinstance(line, list)):
+                return False, "Not a 2d list"
+                        
             # Ensure every line length matches num of lines
             if (len(line) != lines):
-                return False
+                return False, f"line does not match num of lines: {line}"
 
-            # Ensure every value between 0 and 9
+            # Ensure every value between 0 and size
             for val in line:
-                if (val < 0 or val > 9):
-                    return False
+                if ((not isinstance(val, int)) or val < 0 or val > lines):
+                    return False, f"Invalid value: {val}"
 
-        return True
+        return True, "All good"
+
+    def display_grid(self):
+        '''
+        Print grid as a 2d grid
+        '''
+        for line in self.grid:
+            print(" ".join(str(val) for val in line))
