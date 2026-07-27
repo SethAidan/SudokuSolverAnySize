e1533fbedf9331c0ba7f975b4ae51119457d774e
Seth Maxwell <seth.maxwell@student.manchester.ac.uk>
2026-07-27 16:10:16 +0100

    Test solver for valid 9x9 puzzles

--- test_solver.py
+++ test_solver.py
@@ -1,3 +1,32 @@
 import pytest
 from Board import Board
 from SudokuSolver import Solver
+
+def read_grid(filename):
+    grid = []
+    with open(filename, "r") as file:
+        for line in file:
+            grid.append([int(num) for num in line.split(" ")])
+
+    return grid
+
+# Overall tests
+@pytest.mark.parametrize("puzzle_name", [
+    "nine-A",
+    "nine-B",
+    "nine-C",
+    "nine-D",
+    "nine-E",
+    "nine-F",
+    "nine-G",
+])
+
+def test_9(puzzle_name):
+    grid = read_grid(f"test/Puzzles/{puzzle_name}")
+    ans = read_grid(f"test/Solutions/{puzzle_name}")
+
+    bd = Board(grid)
+    sv = Solver(bd)
+    sv.solve()
+
+    assert bd.grid == ans
