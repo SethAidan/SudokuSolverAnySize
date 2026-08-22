5d58d69fa3d4fe79c9d59e160d70690f684d5d78
Seth Maxwell <seth.maxwell@student.manchester.ac.uk>
2026-08-22 15:30:33 +0100

    Extract value from form input

--- web/script.js
+++ web/script.js
@@ -1,8 +1,6 @@
-// Onclick of the button
-document.querySelector("button").onclick = function () {  
-  // Call python's random_python function
-  eel.random_python()(function(number){                      
-    // Update the div with a random number returned by python
-    document.querySelector(".random_number").innerHTML = number;
-  })
+function getSize() {
+    var size = document.getElementById("size").value;
+
+    document.getElementById("get-size").hidden = true;
+    alert(size);
 }
